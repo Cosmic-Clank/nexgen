e17f3d83d54f51202b780c42f700733c4bb30708
Tayyab Sajid <tayyabsajidq41321@gmail.com>
2024-05-21 20:24:49 +0400

    added a lot

--- public/script2.js
+++ public/script2.js
@@ -1,17 +1,17 @@
 gsap.registerPlugin(ScrollTrigger);
 
 document.querySelectorAll(".slidein").forEach((element) => {
-    gsap.from(element, {
-      scrollTrigger: {
-        trigger: element,
-        start: "top bottom",
-        end: "bottom center",
-        toggleActions: "play none none reverse",
-      },
-      x: -200,
-      opacity: 0,
-      duration: 1,
-      ease: "expo.out",
-      delay: 0.5,
-    });
+  gsap.from(element, {
+    scrollTrigger: {
+      trigger: element,
+      start: "top bottom",
+      end: "bottom center",
+      toggleActions: "play none none reverse",
+    },
+    x: -200,
+    opacity: 0,
+    duration: 1,
+    ease: "expo.out",
+    delay: 0,
   });
+});
