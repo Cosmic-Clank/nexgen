17f101510d326588a41e6a65a11228de32cd6ca1
Tayyab Sajid <tayyabsajidq41321@gmail.com>
2024-05-28 00:07:12 +0400

    added: page 4

--- public/script2.js
+++ public/script2.js
@@ -12,6 +12,24 @@ document.querySelectorAll(".slidein").forEach((element) => {
     opacity: 0,
     duration: 1,
     ease: "expo.out",
-    delay: 0.5,
+    delay: 0.3,
+  });
+});
+
+const timelines = document.querySelectorAll("[id^='timeline-']");
+
+timelines.forEach((timeline, index) => {
+  gsap.from(timeline, {
+    scrollTrigger: {
+      trigger: timeline,
+      start: "top bottom",
+      end: "bottom center",
+      toggleActions: "play none none reverse",
+    },
+    y: 100,
+    opacity: 0,
+    duration: 1,
+    ease: "expo.out",
+    delay: 0.5 + index * 0.6,
   });
 });
