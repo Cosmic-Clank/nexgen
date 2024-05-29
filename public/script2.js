23a878831a4bf66cde334be0d355966948f8fd4e
Tayyab Sajid <tayyabsajidq41321@gmail.com>
2024-05-29 20:19:15 +0400

    modified: changes as required

--- public/script2.js
+++ public/script2.js
@@ -15,21 +15,3 @@ document.querySelectorAll(".slidein").forEach((element) => {
     delay: 0.3,
   });
 });
-
-const timelines = document.querySelectorAll("[id^='timeline-']");
-
-timelines.forEach((timeline, index) => {
-  gsap.from(timeline, {
-    scrollTrigger: {
-      trigger: timeline,
-      start: "top bottom",
-      end: "bottom center",
-      toggleActions: "play none none reverse",
-    },
-    y: 100,
-    opacity: 0,
-    duration: 1,
-    ease: "expo.out",
-    delay: 0.2 + index * 0.2,
-  });
-});
